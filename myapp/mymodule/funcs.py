b874339be8ac134b711ea17969f83cfb1d338432
Lalithachennapragada <lalitha.chennapragada@dcmail.ca>
2025-03-14 14:45:31 -0400

    Initial commit with pytest setup

---
+++ myapp/mymodule/funcs.py
@@ -0,0 +1,22 @@
+"""
+funcs.py contains four math functions.
+"""
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a * 1.0 / b
+
+def square_area(side_length):
+    return side_length * side_length
---
+++ tests/mymodule/test_funcs.py
@@ -0,0 +1,29 @@
+import pytest
+
+from myapp.mymodule.funcs import *
+
+
+@pytest.mark.easy_operation
+def test_add():
+    # This test will fail.
+    assert add(4, 8) == 12
+
+@pytest.mark.easy_operation
+def test_subtract():
+    assert subtract(3, 6) == -3
+
+@pytest.mark.difficult_operation
+def test_multiply():
+    assert multiply(4, 5) == 20
+
+@pytest.mark.difficult_operation
+def test_divide():
+    assert divide(56, 8) == 7
+
+@pytest.mark.square_test
+def test_square_area():
+    assert square_area(10) == 71  # Using last two digits of my student ID - 100979071
+    
+
+
+
